f0e8811ee87876e637ead869e6bfa95214c66e89
juan-moresi <jumor701@gmail.com>
2025-03-16 17:16:08 -0300

    agrego clase para conversion de moneda

---
+++ app/CurrencyConverter.js
@@ -0,0 +1,24 @@
+/* Clase para conversiones de moneda */
+class CurrencyConverter {
+    constructor() {
+        // Monedas desde localStorage o predeterminadas
+        this.monedas = JSON.parse(localStorage.getItem('currencies')) || [
+            { codigo: "USD", nombre: "Dólares estadounidenses", tasa: 1.0 },
+            { codigo: "EUR", nombre: "Euros", tasa: 0.96 },
+            { codigo: "ARS", nombre: "Pesos argentinos", tasa: 1057.77 },
+        ];
+        this.supportedCurrencies = this.monedas.map(moneda => moneda.codigo);
+    }
+
+    addCurrency(nombre, codigo, tasa) {
+        codigo = codigo.toUpperCase();
+        if (this.monedas.some(m => m.codigo === codigo)) {
+            throw new Error("Esta moneda ya existe.");
+        }
+        this.monedas.push({ codigo, nombre, tasa: parseFloat(tasa) });
+        this.supportedCurrencies = this.monedas.map(moneda => moneda.codigo);
+    }
+
+    getSupportedCurrenciesText() {
+        return this.supportedCurrencies.join(', ');
+    }
